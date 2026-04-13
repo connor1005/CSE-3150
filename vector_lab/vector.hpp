f97fe244dba67f654578c969e2812bbc7b9e4dc9
Connor Ingram <redeagle225@gmail.com>
2026-04-13 16:35:56 -0400

    vector lab final commit

--- vector_lab/vector.hpp
+++ vector_lab/vector.hpp
@@ -18,44 +18,95 @@ private:
 	void resize_capacity(size_t new_capacity);
 
 public:
+	class Iterator {
+	private:
+		T* current_;
+	public:
+		Iterator(T* ptr) : current_(ptr) {}
+		
+		T& operator*() { return *current_; }
+		
+		Iterator& operator++() {
+			++current_;
+			return *this;
+		}
+		bool operator!=(const Iterator& other) const {
+			return current_ != other.current_;
+		}
+	};
+
+	class ConstIterator {
+	private:
+		const T* current_;
+	public:
+		ConstIterator(const T* ptr) : current_(ptr) {}
+
+		const T& operator*() const { return *current_;}
+		
+		ConstIterator& operator++() {
+			++current_;
+			return *this;
+		}
+
+		bool operator!=(const ConstIterator& other) const {
+			return current_ != other.current_;
+		}
+	};
+
 	Vector();
 	~Vector();
 
 	Vector(const Vector&) = delete;
-	Vector& operator=(const Vector&) = delete;
-	
-	size_t size() const;
-	size_t capacity() const;
-	bool empty() const;
+    	Vector& operator=(const Vector&) = delete;
 
-	void push_back(const T& value);
-	T& at(size_t index);
+    	size_t size() const;
+    	size_t capacity() const;
+    	bool empty() const;
+
+    	void push_back(const T& value);
+    	T& at(size_t index);
+    
+    	T& operator[](size_t index);
+    	const T& operator[](size_t index) const;
+    
+    	bool operator==(const Vector<T>& other) const;
+
+    	Iterator begin();
+    	Iterator end();
+    	ConstIterator begin() const;
+    	ConstIterator end() const;
 };
 
 template <typename T>
-Vector<T>::Vector() : data_(nullptr), size(0), capacity_(0) {}
+requires EqualityComparable<T>
+Vector<T>::Vector() : data_(nullptr), size_(0), capacity_(0) {}
 
 template <typename T>
+requires EqualityComparable<T>
 Vector<T>::~Vector() {
 	delete[] data_;
 }
 
 template <typename T>
+requires EqualityComparable<T>
 size_t Vector<T>::size() const {
 	return size_;
 }
 
 template <typename T>
+requires EqualityComparable<T>
 size_t Vector<T>::capacity() const {
 	return capacity_;
 }
 
 template <typename T>
+requires EqualityComparable<T>
 bool Vector<T>::empty() const {
 	return size_ == 0;
 }
 
 template <typename T>
+requires EqualityComparable<T>
 void Vector<T>::resize_capacity(size_t new_capacity) {
 	T* new_data = new T[new_capacity];
 	for (size_t i = 0; i < size_; ++i) {
@@ -67,6 +118,7 @@ void Vector<T>::resize_capacity(size_t new_capacity) {
 }
 
 template <typename T>
+requires EqualityComparable<T>
 void Vector<T>::push_back(const T& value) {
 	if (size_ == capacity_) {
 		size_t new_cap = (capacity_ == 0) ? 1 : capacity_ * 2;
@@ -77,6 +129,7 @@ void Vector<T>::push_back(const T& value) {
 }
 
 template <typename T>
+requires EqualityComparable<T>
 T& Vector<T>::at(size_t index) {
 	if (index >= size_) {
 		throw std::out_of_range("Vector index out of bounds!");
@@ -85,16 +138,19 @@ T& Vector<T>::at(size_t index) {
 }
 
 template <typename T>
+requires EqualityComparable<T>
 T& Vector<T>::operator[](size_t index) {
 	return data_[index];
 }
 
 template <typename T>
-const T& Vector<T>::opertor[](size_t index) const {
+requires EqualityComparable<T>
+const T& Vector<T>::operator[](size_t index) const {
 	return data_[index];
 }
 
 template <typename T>
+requires EqualityComparable<T>
 bool Vector<T>::operator==(const Vector<T>& other) const {
 	if (size_ != other.size_) {
 		return false;
@@ -105,4 +161,28 @@ bool Vector<T>::operator==(const Vector<T>& other) const {
 		}
 	}
 	return true;
+}
+
+template <typename T>
+requires EqualityComparable<T>
+typename Vector<T>::Iterator Vector<T>::begin() {
+    return Iterator(data_);
+}
+
+template <typename T>
+requires EqualityComparable<T>
+typename Vector<T>::Iterator Vector<T>::end() {
+    return Iterator(data_ + size_);
+}
+
+template <typename T>
+requires EqualityComparable<T>
+typename Vector<T>::ConstIterator Vector<T>::begin() const {
+    return ConstIterator(data_);
+}
+
+template <typename T>
+requires EqualityComparable<T>
+typename Vector<T>::ConstIterator Vector<T>::end() const {
+    return ConstIterator(data_ + size_);
 }
